e8156b1feb7b6a083cf2ec992eb89167eeda416c
RonPer17 <153912624+RonPer17@users.noreply.github.com>
2023-12-15 22:04:49 +0800

    Add files via upload

---
+++ admin/profit print.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Invoice</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="license" href="https://www.opensource.org/licenses/mit-license/">
+		<script src="script.js"></script>
+
+<button id="printButton" class="btn btn-primary">Print Profit Report</button>
